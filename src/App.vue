460b0f11c53668a1ede01f5532fb7fa57ce1237c
BrunoDelBer <brunomar.39@gmail.com>
2025-05-30 12:18:22 -0600

    Creacion de las vistas y configuracion del Router

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,37 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+   <div class="flex flex-col h-screen">
+      <!-- Header -->
+      <header class="flex items-center h-14 px-4 border-b border-gray-300 sm:h-16 md:px-6 lg:px-8">
+         <div>
+            <a class="flex items-center gap-2 font-semibold" href="#">
+               <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="40" height="40" />
+            </a>
+         </div>
+         <nav class="ml-auto space-x-4 flex items-center h-10 sm:space-x-6">
+            <RouterLink to="/"> Home </RouterLink>
+            <RouterLink to="/features"> Features </RouterLink>
+            <RouterLink to="/pricing"> Pricing </RouterLink>
+            <RouterLink to="/contact"> Contact </RouterLink>
+         </nav>
+      </header>
+      <!-- Fin Header -->
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <!-- Main -->
+      <main class="flex-1 flex items-center justify-center py-6">
+         <RouterView></RouterView>
+      </main>
+      <!-- Fin Main -->
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+      <!-- Footer -->
+      <footer class="flex items-center h-14 px-4 border-t border-gray-300 sm:h-16 md:px-6 lg:px-8">
+         <p class="flex-1 text-sm text-gray-500 text-center">
+            © {{ new Date().getFullYear() }} Pedros Corporation. Derechos reservados
+         </p>
+      </footer>
+      <!-- Fin Footer -->
+   </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
